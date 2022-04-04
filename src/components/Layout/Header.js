2f9fe4003f597dcc369140392ce07109b6500444
Djordje Knezevic <starwarrior@mycity.rs>
2022-04-04 20:57:01 +0200

    Fix import in Modal.js and manage Cart state in App.js

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -4,11 +4,11 @@ import classes from './Header.module.css';
 import mealsImage from '../../assets/meals.jpg';
 import HeaderCartButton from './HeaderCartButton';
 
-const Header = props => {
+const Header = (props) => {
     return <Fragment>
       <header className={classes.header}>
           <h1>Meals</h1>
-          <HeaderCartButton />
+          <HeaderCartButton onClick={props.onShowCart} />
       </header>
       <div className={classes['main-image']}>
           <img src={mealsImage} alt='A table full of delicious meals!' />
